625b636856072d0d8fdba5333cbfe712058c4f48
NAMGYAL DORJEE <namgyaldorjee@MacBookPro.lan>
2025-07-23 23:04:19 -0400

    Fix bad route syntax

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,46 +1,41 @@
 
 import { Server } from "socket.io";
-
-import http from 'http';
-
-import express from 'express';
+import http from "http";
+import express from "express";
 
 const app = express();
-
 const server = http.createServer(app);
 
-
 const io = new Server(server, {
-    cors:{
-        origin: ['http://localhost:5173'],
-    }
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
 });
 
-export function getReceiverSocketId(userId){
-    return userSocketMap[userId];
-};
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
 
 // used to store online users
-const userSocketMap = {};
+const userSocketMap = {}; // {userId: socketId}
 
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
 
-io.on("connection", (socket)=>{
-    console.log("A user connected.", socket.id);
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
 
-    const userId = socket.handshake.query.userId;
-    if(userId) userSocketMap[userId] = socket.id; //update the userId
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    //to all clients including sender
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
-    socket.on("disconnect", ()=>{
-        console.log('A user disconnected.', socket.id);
-        delete userSocketMap[userId];
-        io.emit("getOnLineUsers", Object.keys(userSocketMap));
-    })
-})
-
-export {io, app, server};
+export { io, app, server };
 
 
 
